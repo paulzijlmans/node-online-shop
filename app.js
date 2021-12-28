92d74bf8d25177cfa068972753e1c2a0b582b093
Paul Zijlmans <paul.zijlmans@devolksbank.nl>
2021-12-28 20:45:16 +0100

    return a 404 page

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page not found</h1>');
+    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
 });
 
 app.listen(3000);
