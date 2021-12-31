d5d50a77e49250551eaf95e8bfd17bd5f6873f38
Paul Zijlmans <paul.zijlmans@devolksbank.nl>
2021-12-31 14:40:05 +0100

    use pug

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', 'page-not-found.html'));
+    res.status(404).render('page-not-found', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
