44636cd6183f8be98190671343d74a75129f6324
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-02-14 20:52:08 +0100

    Register the routes

--- controllers/error.js
+++ controllers/error.js
@@ -1,3 +1,3 @@
 exports.getPageNotFound = (req, res, next) => {
-  res.status(404).render('page-not-found', { pageTitle: 'Page Not Found', path: '' });
+  res.status(404).render('page-not-found', { pageTitle: 'Page Not Found', path: '/page-not-found' });
 };
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,13 @@
 const express = require('express');
 
-const productsController = require('../controllers/products');
+const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-router.get('/products', productsController.getAdminProducts);
+router.get('/add-product', adminController.getAddProduct);
 
-router.get('/add-product', productsController.getAddProduct);
+router.get('/products', adminController.getProducts);
 
-router.post('/add-product', productsController.postAddProduct);
+router.post('/add-product', adminController.postAddProduct);
 
 module.exports = router;
