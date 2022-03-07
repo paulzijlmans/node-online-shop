9d52f264a3a9332affa9f843b1a7a1863d4ade10
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-03-07 16:49:11 +0100

    Sequelize: Create Dummy User - add dummy user to request

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findByPk(1);
+  User.findByPk(1)
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err));;
 });
 
 app.use('/admin', adminRoutes);
