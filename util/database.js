60046e4f4c94f11231f54683eb69a5370fe30980
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-03-11 20:42:51 +0100

    MongoDB: Create Database Connection

--- util/database.js
+++ util/database.js
@@ -1,11 +1,28 @@
 require('dotenv').config();
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
+const { MONGO_INITDB_ROOT_USERNAME, MONGO_INITDB_ROOT_PASSWORD, MONGO_INITDB_DATABASE } = process.env;
+
+let _db;
 
 const mongoConnect = () => {
-  return MongoClient.connect(`mongodb://${process.env.MONGO_INITDB_ROOT_USERNAME}:${process.env.MONGO_INITDB_ROOT_PASSWORD}@mongodb`)
-    .then(console.log('Connected to MongoDB!'))
-    .catch(err => console.log(err));
+  return MongoClient.connect(`mongodb://${MONGO_INITDB_ROOT_USERNAME}:${MONGO_INITDB_ROOT_PASSWORD}@mongodb/${MONGO_INITDB_DATABASE}?authSource=admin`)
+    .then(client => {
+      console.log('Connected to MongoDB!');
+      _db = client.db();
+    })
+    .catch(err => {
+      console.log(err);
+      throw err;
+    });
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  console.log('No database found!');
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
