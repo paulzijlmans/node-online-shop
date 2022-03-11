6229e125808cb78d58d8b5076d995d73e2c303ca
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-03-11 09:30:46 +0100

    Sequelize: Add Existing Products and Retrieve Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -55,6 +55,7 @@ exports.getCart = (req, res, next) => {
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
   let fetchedCart;
+  let newQuantity = 1;
   req.user.getCart()
     .then(cart => {
       fetchedCart = cart;
@@ -65,22 +66,20 @@ exports.postCart = (req, res, next) => {
       if (products.length > 0) {
         product = products[0];
       }
-      let newQuantity = 1;
       if (product) {
-        // ...
+        const oldQuantity = product.cartItem.quantity;
+        newQuantity = oldQuantity + 1;
+        return product;
       }
-      return Product.findByPk(productId)
-        .then(prod => fetchedCart.addProduct(prod, {
-          through: { quantity: newQuantity }
-        }))
-        .catch(err => console.log(err));
+      return Product.findByPk(productId);
     })
-    .then(() => res.redirect('/'))
+    .then(product => {
+      return fetchedCart.addProduct(product, {
+        through: { quantity: newQuantity }
+      });
+    })
+    .then(() => res.redirect('/cart'))
     .catch(err => console.log(err));
-  // Product.findById(productId, (product) => {
-  //   Cart.addProduct(productId, product.price);
-  // });
-  // res.redirect('/cart');
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
