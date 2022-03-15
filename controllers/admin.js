326e97d4e6b27b7eb63db1b20fdc5a8061120cd6
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-03-15 20:08:56 +0100

    MongoDB: Use Database Connection

--- controllers/admin.js
+++ controllers/admin.js
@@ -23,68 +23,68 @@ exports.postAddProduct = (req, res, next) => {
     .catch(err => console.log(err));
 }
 
-exports.getEditProduct = (req, res, next) => {
-  const editMode = req.query.edit;
-  if (!editMode) {
-    return res.redirect('/');
-  }
-  const productId = req.params.productId;
-  req.user.getProducts({ where: { id: productId } })
-    .then(products => {
-      const product = products[0];
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product
-      });
-    })
-    .catch(err => console.log(err));
-};
+// exports.getEditProduct = (req, res, next) => {
+//   const editMode = req.query.edit;
+//   if (!editMode) {
+//     return res.redirect('/');
+//   }
+//   const productId = req.params.productId;
+//   req.user.getProducts({ where: { id: productId } })
+//     .then(products => {
+//       const product = products[0];
+//       if (!product) {
+//         return res.redirect('/');
+//       }
+//       res.render('admin/edit-product', {
+//         pageTitle: 'Edit Product',
+//         path: '/admin/edit-product',
+//         editing: editMode,
+//         product: product
+//       });
+//     })
+//     .catch(err => console.log(err));
+// };
 
-exports.postEditProduct = (req, res, next) => {
-  const productId = req.body.productId;
-  const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
-  const updatedImageUrl = req.body.imageUrl;
-  const updatedDescription = req.body.description;
-  Product.findByPk(productId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.imageUrl = updatedImageUrl;
-      product.description = updatedDescription;
-      return product.save();
-    })
-    .then(() => {
-      res.redirect('/admin/products');
-    })
-    .catch(err => console.log(err));
-}
+// exports.postEditProduct = (req, res, next) => {
+//   const productId = req.body.productId;
+//   const updatedTitle = req.body.title;
+//   const updatedPrice = req.body.price;
+//   const updatedImageUrl = req.body.imageUrl;
+//   const updatedDescription = req.body.description;
+//   Product.findByPk(productId)
+//     .then(product => {
+//       product.title = updatedTitle;
+//       product.price = updatedPrice;
+//       product.imageUrl = updatedImageUrl;
+//       product.description = updatedDescription;
+//       return product.save();
+//     })
+//     .then(() => {
+//       res.redirect('/admin/products');
+//     })
+//     .catch(err => console.log(err));
+// }
 
-exports.getProducts = (req, res, next) => {
-  req.user.getProducts()
-    .then(products => {
-      res.render('admin/products', {
-        prods: products,
-        pageTitle: 'Admin Products',
-        path: '/admin/products'
-      });
-    })
-    .catch(err => console.log(err));
-}
+// exports.getProducts = (req, res, next) => {
+//   req.user.getProducts()
+//     .then(products => {
+//       res.render('admin/products', {
+//         prods: products,
+//         pageTitle: 'Admin Products',
+//         path: '/admin/products'
+//       });
+//     })
+//     .catch(err => console.log(err));
+// }
 
-exports.postDeleteProduct = (req, res, next) => {
-  const productId = req.body.productId;
-  Product.findByPk(productId)
-    .then(product => {
-      return product.destroy()
-    })
-    .then(() => {
-      res.redirect('/admin/products');
-    })
-    .catch(err => console.log(err));
-}
+// exports.postDeleteProduct = (req, res, next) => {
+//   const productId = req.body.productId;
+//   Product.findByPk(productId)
+//     .then(product => {
+//       return product.destroy()
+//     })
+//     .then(() => {
+//       res.redirect('/admin/products');
+//     })
+//     .catch(err => console.log(err));
+// }
