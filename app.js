66afbc86f43089b720285b1c9033e6e0e1c48de2
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-03-16 12:41:53 +0100

    MongoDB: Create New User

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/User');
 
 const app = express();
 
@@ -13,18 +14,18 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const req = require('express/lib/request');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  // User.findByPk(1)
-  //   .then(user => {
-  //     req.user = user;
-  //     next();
-  //   })
-  //   .catch(err => console.log(err));
-  next();
+  User.find()
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => console.log(err));
 });
 
 app.use('/admin', adminRoutes);
@@ -33,5 +34,13 @@ app.use(shopRoutes);
 app.use(errorController.getPageNotFound);
 
 mongoConnect()
+  .then(() => User.find())
+  .then(user => {
+    if (!user) {
+      const newUser = new User('Paul', 'paul@test.com');
+      return newUser.save()
+    }
+    return user;
+  })
   .then(() => app.listen(3000))
   .catch(err => console.log(err));
--- models/user.js
+++ models/user.js
@@ -1,21 +1,29 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false
+class User {
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
   }
-});
+
+  save() {
+    const db = getDb();
+    return db.collection('users')
+      .insertOne(this);
+  }
+
+  static findById(userId) {
+    const db = getDb();
+    return db.collection('users')
+      .findOne({ _id: new mongodb.ObjectId(userId) });
+  }
+
+  static find() {
+    const db = getDb();
+    return db.collection('users')
+      .findOne();
+  }
+}
 
 module.exports = User;
