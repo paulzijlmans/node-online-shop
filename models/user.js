05f2924f1b579c3c0cf525806165c9f45a77963b
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-03-17 08:38:26 +0100

    MongoDB: Cart Items

--- models/user.js
+++ models/user.js
@@ -2,15 +2,29 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection('users')
       .insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cartItem => {
+    //   return cartItem._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db.collection('users').updateOne(
+      { _id: new mongodb.ObjectId(this._id) },
+      { $set: { cart: updatedCart } }
+    );
   }
 
   static findById(userId) {
