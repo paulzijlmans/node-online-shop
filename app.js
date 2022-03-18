8e299e8bc11a074d34220df90869c4f535b9553f
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-03-18 16:26:24 +0100

    Sessions & Cookies: Use Session Middleware

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const session = require('express-session');
 
 const errorController = require('./controllers/error');
 const database = require('./util/database');
@@ -18,6 +19,10 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({ 
+  secret: 'my secret', 
+  resave: false, 
+  saveUninitialized: false }));
 
 app.use((req, res, next) => {
   User.findOne()
