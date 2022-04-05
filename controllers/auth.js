e23948473f2994c1c5452860fb5977660003c549
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-04-05 11:16:52 +0200

    Validation: Add Conditional CSS Class

--- controllers/auth.js
+++ controllers/auth.js
@@ -27,7 +27,8 @@ exports.getSignup = (req, res, next) => {
     path: '/signup',
     pageTitle: 'Signup',
     errorMessage: getErrorMessage(req),
-    oldInput: { email: '', password: '', confirmPassword: '' }
+    oldInput: { email: '', password: '', confirmPassword: '' },
+    validationErrors: []
   });
 };
 
@@ -83,7 +84,8 @@ exports.postSignup = (req, res, next) => {
         path: '/signup',
         pageTitle: 'Signup',
         errorMessage: errors.array()[0].msg,
-        oldInput: { email: email, password: password, confirmPassword: req.body.confirmPassword }
+        oldInput: { email: email, password: password, confirmPassword: req.body.confirmPassword },
+        validationErrors: errors.array()
       });
   }
 
