496c5614e5ce99c8e0dca8118edbd45307113001
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-04-11 18:04:53 +0200

    File Upload & Download: Generate PDF

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+const PDFDocument = require('pdfkit');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 const { handleError } = require('../util/error-handler');
@@ -116,10 +118,24 @@ exports.getInvoice = (req, res, next) => {
       }
       const invoiceName = 'invoice-' + orderId + '.pdf';
       const invoicePath = path.join('data', 'invoices', invoiceName);
-      const file = fs.createReadStream(invoicePath);
+
+      const pdfDoc = new PDFDocument();
       res.setHeader('Content-Type', 'application/pdf');
       res.setHeader('Content-Disposition', 'inline; filename="' + invoiceName + '"');
-      file.pipe(res);
+      pdfDoc.pipe(fs.createWriteStream(invoicePath));
+      pdfDoc.pipe(res);
+
+      pdfDoc.fontSize(26).text('Invoice', { underline: true });
+      pdfDoc.text('--------------------');
+      let totalPrice = 0;
+      order.products.forEach(prod => {
+        totalPrice += (prod.quantity * prod.product.price);
+        pdfDoc.fontSize(14).text(prod.product.title + ' - ' + prod.quantity + ' x $ ' + prod.product.price);
+      });
+      pdfDoc.text('---');
+      pdfDoc.fontSize(20).text('Total Price: $' + totalPrice);
+
+      pdfDoc.end();
     })
     .catch(err => next(err));
 };
