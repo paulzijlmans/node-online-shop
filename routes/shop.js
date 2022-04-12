31590f4ee9405f20012099276ca99051ea0a027c
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-04-12 21:28:15 +0200

    Payments: Stripe

--- routes/shop.js
+++ routes/shop.js
@@ -17,9 +17,11 @@ router.post('/cart', isAuth, shopController.postCart);
 
 router.post('/cart-delete-item', isAuth, shopController.postCartDeleteProduct);
 
-router.post('/checkout', isAuth, shopController.getCheckout);
+router.get('/checkout', isAuth, shopController.getCheckout);
 
-router.post('/create-order', isAuth, shopController.postOrder);
+router.get('/checkout/success', shopController.getCheckoutSuccess);
+
+router.get('/checkout/cancel', shopController.getCheckout);
 
 router.get('/orders', isAuth, shopController.getOrders);
 
